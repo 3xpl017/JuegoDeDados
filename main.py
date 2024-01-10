f3b67926aba256c867ca0e693bfbfd875ff21304
Phoenix <153451582+3xpl017@users.noreply.github.com>
2024-01-10 16:41:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random, os
+import random, os, time
 from colorama import Fore, Back
 
 numeros = [1, 2, 3, 4, 5, 6]
@@ -7,10 +7,13 @@ aleatorio = random.choice(numeros)
 
 generados = random.randint(1, 6)
 
+os.system('clear')
+
 while True:
     if aleatorio == generados:
         print(Fore.GREEN + f'[!] Los números {generados} y {aleatorio} coinciden.\n')
         break
+        os.system('clear')
 
     else:
         print(Fore.RED + f'[!] Los números {generados} y {aleatorio} no coinciden.')
