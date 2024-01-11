d189b6a7cf0d423e9769a1134e0fc45e947d9a75
Phoenix <153451582+3xpl017@users.noreply.github.com>
2024-01-11 17:05:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install python3-pip
 pip3 install random 
 pip3 install colorama
 pip3 install os
 pip3 install time
+echo ======================
+echo [~]
+sudo bash install.sh
